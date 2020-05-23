04d2adaec6483b0cf835e1befeee720c994eb8ad
Ringo Yungpo Kao <ringoyungpo@qq.com>
2020-05-23 18:08:00 +0800

    files api complete

--- src/main/java/io/github/ringoyungpo/filecloud/FileRepository.java
+++ src/main/java/io/github/ringoyungpo/filecloud/FileRepository.java
@@ -2,10 +2,12 @@ package io.github.ringoyungpo.filecloud;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * @author ringo
  */
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
 @RepositoryRestResource(collectionResourceRel = "files", path = "files")
 public interface FileRepository extends PagingAndSortingRepository<File, Long> {
 }
