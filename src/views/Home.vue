08e4524af4b1569bf1f7acbc54018a9876adc0ec
Ringo Yungpo Kao <ringoyungpo@qq.com>
2020-05-24 12:51:15 +0800

    file page finished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,85 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <el-main>
+    <el-table
+      :data = "files"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="name"
+        label="name"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="content"
+        label="content">
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        label="操作"
+        width="100">
+        <template slot-scope="scope">
+          <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+          <el-button type="text" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="(size)=>{page.size = size; search()}"
+      @current-change="(current)=>{page.current = current; page.number = current - 1; search()}"
+      :current-page="page.current"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="page.size"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="page.totalElements">
+    </el-pagination>
+  </el-main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 import { createClient } from "hal-rest-client";
+import { stringify } from "qs"
+
+const client = createClient("/api");
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   },
   async mounted() {
-    const client = createClient("/api");
-    const fileCreated = await client.create("/files", { id: null, name: "Thomas", content: "jkdsjjssdksdfsfd" });
-    fileCreated.prop('name', "alex")
-    const fileUpdated = await fileCreated.update()
-    const filesResource = await client.fetchResource("/files");
-    const files = filesResource.prop("files")
-    console.log({fileCreated,fileUpdated,files})
+    // let count = 23;
+    // do {
+    //   await client.create("/files", { id: null, name: "Thomas", content: "jkdsjjssdksdfsfd" });
+    // } while(--count > 0)
+    await this.search()
+  },
+  methods: {
+    async search(){
+      const {number, size} = this.page
+      const filesPageResource = await client.fetchResource(`/files?${stringify({page: number,size})}`);
+      const {files, page} = filesPageResource.props
+      this.files = files.map(file => file.props)
+      this.page = {
+        ...page,
+        current: page.number + 1
+      }
+    },
+    handleClick(row) {
+      console.log(row)
+      const {href} = row
+      this.$router.push({ name: 'Editor', params: { href }})
+    }
+  },
+  data() {
+    return {
+      files: [],
+      page: {
+        current: 1,
+        number: 0,
+        size: 10,
+        totalElements: 0
+      },
+    }
   }
 }
 </script>
