087148455cdb9ddae1e33828303c5a83e1bebdb7
Ringo Yungpo Kao <ringoyungpo@qq.com>
2020-05-24 12:52:37 +0800

    expose id for files api

---
+++ src/main/java/io/github/ringoyungpo/filecloud/SpringDataRestConfig.java
@@ -0,0 +1,19 @@
+package io.github.ringoyungpo.filecloud;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class SpringDataRestConfig {
+    @Bean
+    public RepositoryRestConfigurer repositoryRestConfigurer() {
+        return new RepositoryRestConfigurer() {
+            @Override
+            public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+                config.exposeIdsFor(File.class);
+            }
+        };
+    }
+}
